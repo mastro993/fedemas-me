3b837fcc57c74cd5850cf118c7e3079da356dca7
Federico Mastrini <federicomastrini93@gmail.com>
2024-12-07 17:41:31 +0100

    chore: remove redux

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,23 +1,14 @@
-import {ThemeProvider} from 'styled-components'
 import React from "react";
-import {useThemeManager} from "./state/user/hooks";
-import {Provider} from "react-redux";
-import store from "./state";
-import {DarkTheme, LightTheme} from "./theme";
+import { ThemeProvider } from "styled-components";
+import { DarkTheme, LightTheme } from "./theme";
 
 const ThemeProviderWrapper: React.FC = (props) => {
-    const {isDark} = useThemeManager()
-    return <ThemeProvider theme={isDark ? DarkTheme : LightTheme} {...props} />
-}
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  return <ThemeProvider theme={isDark ? DarkTheme : LightTheme} {...props} />;
+};
 
-const Providers: React.FC = ({children}) => {
-    return (
-        <Provider store={store}>
-            <ThemeProviderWrapper>
-                {children}
-            </ThemeProviderWrapper>
-        </Provider>
-    )
-}
+const Providers: React.FC = ({ children }) => {
+  return <ThemeProviderWrapper>{children}</ThemeProviderWrapper>;
+};
 
-export default Providers
+export default Providers;
